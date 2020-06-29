1fcd181a8b7fd3a57f813680b01cc29892bb64b8
Branko Gluvajić <gluvajicbranko@gmail.com>
2020-06-29 23:12:55 +0200

    Loading Data / Visualization

--- ORI_PROJ_1.1/main.py
+++ ORI_PROJ_1.1/main.py
@@ -1,11 +1,36 @@
 import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import sys
 
 def load_data():
-    df = pd.read_csv("credit_card_data.csv")
+    df = pd.read_csv(sys.argv[2])
     return df
 
 
 if __name__ == '__main__':
     df = load_data()
 
-    print(type(df))
+    # Izbacujemo CUST_ID kolonu, jer sadrzi tekstualne vrednosti koje nisu od znacaja za model
+    df = df.drop("CUST_ID", axis='columns')
+
+    # Funkcija koja izbacuje sve redove koje sadrze NaN vrednost u nekom polju
+    df = df.dropna()
+
+    # -------------------------------------- DATA LOADED FULLY ---------------------------------------- #
+
+
+    #scaler = StandardScaler()
+    #df_norm = pd.DataFrame(scaler.fit_transform(df.astype(float)))
+
+
+    #kmeans = KMeans(n_clusters=2, max_iter=100)
+    #kmeans.fit(df)
+
+    # type(balance) = pandas.core.series.Series
+    balance = df["BALANCE"]
+    purchases = df["PURCHASES"]
+
+    plt.scatter(balance, purchases)
+    plt.show()
